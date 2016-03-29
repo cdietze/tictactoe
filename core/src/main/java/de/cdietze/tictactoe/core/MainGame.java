21394300d79a31ad75d8d5dabe8f47c7a8708faf
Christoph Dietze <christoph.dietze@gmail.com>
2016-03-29 21:54:09 +0200

    Formatting

--- core/src/main/java/de/cdietze/tictactoe/core/MainGame.java
+++ core/src/main/java/de/cdietze/tictactoe/core/MainGame.java
@@ -10,30 +10,31 @@ import tripleplay.game.trans.SlideTransition;
 
 public class MainGame extends SceneGame {
 
-  public final ScreenStack screens = new ScreenStack(this, rootLayer) {
-    @Override
-    protected Transition defaultPushTransition() {
-      return new SlideTransition(this).left();
+    public final ScreenStack screens = new ScreenStack(this, rootLayer) {
+        @Override
+        protected Transition defaultPushTransition() {
+            return new SlideTransition(this).left();
+        }
+
+        @Override
+        protected Transition defaultPopTransition() {
+            return new SlideTransition(this).right();
+        }
+    };
+
+    public MainGame(Platform plat) {
+        super(plat, 33); // update our "simulation" 33ms (30 times per second)
+
+        // create and add background image layer
+        Image bgImage = plat.assets().getImage("images/bg.png");
+        ImageLayer bgLayer = new ImageLayer(bgImage);
+        // scale the background to fill the screen
+        bgLayer.setSize(plat.graphics().viewSize);
+        rootLayer.add(bgLayer);
+
+        // Register Pointer so we can handle any pointer input (clicks, mouse/touch events etc.)
+        new Pointer(plat, rootLayer, true);
+
+        screens.push(new BoardScreen(this));
     }
-    @Override
-    protected Transition defaultPopTransition() {
-      return new SlideTransition(this).right();
-    }
-  };
-
-  public MainGame(Platform plat) {
-    super(plat, 33); // update our "simulation" 33ms (30 times per second)
-
-    // create and add background image layer
-    Image bgImage = plat.assets().getImage("images/bg.png");
-    ImageLayer bgLayer = new ImageLayer(bgImage);
-    // scale the background to fill the screen
-    bgLayer.setSize(plat.graphics().viewSize);
-    rootLayer.add(bgLayer);
-
-    // Register Pointer so we can handle any pointer input (clicks, mouse/touch events etc.)
-    new Pointer(plat, rootLayer, true);
-
-    screens.push(new BoardScreen(this));
-  }
 }
--- core/src/main/java/de/cdietze/tictactoe/core/util/MyScreen.java
+++ core/src/main/java/de/cdietze/tictactoe/core/util/MyScreen.java
@@ -6,19 +6,21 @@ import tripleplay.anim.Animator;
 import tripleplay.game.ScreenStack;
 
 public class MyScreen extends ScreenStack.UIScreen {
-  public final MainGame game;
-  /**
-   * An animator that's updated on every update(), thus can be used for game logic. This supplements {@link
-   * tripleplay.ui.Interface#anim} which is updated on paint().
-   */
-  public final Animator updateAnim = new Animator();
+    public final MainGame game;
+    /**
+     * An animator that's updated on every update(), thus can be used for game logic. This supplements {@link
+     * tripleplay.ui.Interface#anim} which is updated on paint().
+     */
+    public final Animator updateAnim = new Animator();
 
-  public MyScreen(MainGame game) {
-    super(game.plat);
-    this.game = game;
-    update.connect(updateAnim.onPaint);
-  }
+    public MyScreen(MainGame game) {
+        super(game.plat);
+        this.game = game;
+        update.connect(updateAnim.onPaint);
+    }
 
-  @Override
-  public Game game() { return game; }
+    @Override
+    public Game game() {
+        return game;
+    }
 }
